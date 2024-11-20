1ca61ba2d3461473df610b565193751fd7a4fcbb
waleedsaleem <waleed.saleem@cityscapeone.com>
2024-11-20 14:43:41 +0500

    payload validation added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties not defined in the DTO
+      forbidNonWhitelisted: true, // Throw an error if unknown properties are sent
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
