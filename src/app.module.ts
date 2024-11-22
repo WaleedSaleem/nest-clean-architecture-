5954507ef5bac88aa675b987e793eb1cb682e841
waleedsaleem <waleed.saleem@cityscapeone.com>
2024-11-22 18:31:41 +0500

    logger added (format not working)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PresentationModule } from './presentation/presentation.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   imports: [
+    LoggerModule,
     MongooseModule.forRoot('mongodb://localhost:27017/local'),
     DatabaseModule,
     PresentationModule,
