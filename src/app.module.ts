9fef36e44b82ad7e99bb6eea89314d9e857fb69f
waleedsaleem <waleed.saleem@cityscapeone.com>
2025-06-13 19:53:28 +0500

    feat: integrate TypeORM with MySQL and add database configuration
    - Added TypeORM support for MySQL in app.module.ts with async configuration using ConfigService.
    - Removed the PropertyModel class as it is no longer needed.
    - Introduced a new database configuration file to manage database connection settings.
    - Updated package.json to include necessary dependencies for TypeORM and configuration.
    - Configured TypeOrmModule in InfrastructureModule to use PropertyModel.
    - Adjusted tsconfig.json to include and exclude specific files for better type checking.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,34 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PresentationModule } from './presentation/presentation.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { DomainModule } from './domain/domain.module';
 import { ApplicationModule } from './application/application.module';
 import { LoggerModule } from './logger/logger.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     LoggerModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/local'),
+    TypeOrmModule.forRootAsync({
+      name: 'replica',
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        name: 'replica',
+        host: configService.get<string>('database.mysqlReplicaHost'),
+        username: configService.get<string>('database.mysqlUserName'),
+        port: configService.get<number>('database.mysqlPort'),
+        password: configService.get<string>('database.mysqlPassword'),
+        database: configService.get<string>('database.mysqlDatabase'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
     InfrastructureModule,
     PresentationModule,
     DomainModule,
